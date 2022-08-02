f5ea81ca6e11edf9adccf06921830c0b2c2cf2b8
Parth Goyal <68484923+Parth-Goyal11@users.noreply.github.com>
2022-08-02 11:49:55 -0400

    Fix minor problems in ecov test teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebCam/Test_teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebCam/Test_teleop.java
@@ -6,24 +6,34 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.WebCam.Pipelines.Helpers.VisionObject;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 @TeleOp(name = "Ecov", group = "robot")
 public class Test_teleop extends LinearOpMode {
-  Camera camera = new Camera(hardwareMap);
 
   @Override
   public void runOpMode() throws InterruptedException {
+    Camera camera = new Camera(hardwareMap);
     camera.switchToFFPipleine();
     telemetry.addLine("Status: Initialized");
+    telemetry.update();
     waitForStart();
 
     while(opModeIsActive()){
       ArrayList<VisionObject> objects = camera.getObjects();
-      if (objects.size() > 0) {
-        VisionObject o = objects.get(0);
-        telemetry.addLine("Pixel coords (x,y) " + o.centerX + " " + o.centerY);
-        telemetry.addLine("Inch coords (x,y) " + o.inchX + " " + o.inchY);
+      try {
+        if (objects.size() > 0) {
+          Collections.sort(objects);
+          VisionObject o = objects.get(objects.size()-1);
+          if(o!=null) {
+            telemetry.addLine("Pixel coords (x,y) " + o.centerX + " " + o.centerY);
+            telemetry.addLine("Inch coords (x,y) " + o.inchX + " " + o.inchY);
+          }
+        }
+      }catch (Exception e){
+        telemetry.addLine("No Object Found");
       }
+      telemetry.update();
     }
   }
 }
